080c719527cc081863937a6d0712994e7b484499
Piyush R Kapile <piyushkapile2002@gmail.com>
2024-10-20 15:29:19 +0530

    add updated hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main()
 {
     cout<<"Hello from Piyush"<<endl;
+    string name;
+    cout<<"Enter your name "<<endl;
+    cin>>name;
+    cout<<"Hello All !!! , from "<<name<<endl;
     return 0;
 }
